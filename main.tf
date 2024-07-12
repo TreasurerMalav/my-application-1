c7d575b9043fc2193cb7850dc6e192894f80701e
Malav Treasurer <malav.treasurer@gmail.com>
2024-07-12 10:26:04 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
-I am Malav
-My name is abc
-]\kpokokbckdbnck
+terraform {
+  backend "s3" {
+    bucket = "my-application-1"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+}
