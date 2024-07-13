35980d4e74bad787eb475be0ead838bb5cdb281d
Malav Treasurer <malav.treasurer@gmail.com>
2024-07-13 07:01:16 +0000

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    bucket = "my-application-1"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AppServer"
+  }
+}
